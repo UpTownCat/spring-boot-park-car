78324ef093ee9040473b4eedc0c819c0619f564d
UpTownCat <158668658@qq.com>
2017-07-07 00:03:52 +0800

    环境搭建以及测试

---
+++ src/main/java/com/example/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+}
